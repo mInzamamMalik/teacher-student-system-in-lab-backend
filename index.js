150f80a44cf3a2a8c33f4ba36ce9c90f2f102765
inzamam malik <malikasinger@gmail.com>
2015-12-05 21:36:13 +0500

    finally arrived to a milestone

--- index.js
+++ index.js
@@ -1,33 +1,72 @@
 /**
  * Created by sayla on 2/12/2015.
  */
+
 var express = require("express");
 var bodyParser = require("body-parser");
-var port = 3000;
-var admin = {username : "admin", password : "password"};
+var cors = require("cors");
 
+var allUsers = [
+    {
+        email: "malikasinger@gmail.com",
+        password: "pakistan",
+        adminStatus: 'admin'
+    },
+    {
+        email: "admin@admin.com",
+        password: "admin",
+        adminStatus: 'admin'
+    },
+    {
+        email: "user@user.com",
+        password: "user",
+        adminStatus: 'user'
+    }
+];
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-app.post('/login', function(req , res) {
+app.use(bodyParser.json({}));
 
-    /*var name = req.body.name;
-    var email = req.body.email;
-    console.log('Name: ' + name);
-    console.log('Email: ' + email);
-    res.json(req.body);*/
-    res.redirect("  /home");
+
+app.post("/login", function (req, res) {
+
+    var logedin = null;
+    console.log("post hitted " + logedin);
+
+    for (var i = 0; i < allUsers.length; i++) {
+
+        if (req.body.email == allUsers[i].email) {
+            if (req.body.password == allUsers[i].password) {
+                logedin = i;
+                console.log(allUsers[i].email + "   " + allUsers[i].password);
+                break;
+            }
+
+        }
+    }
+
+
+    if (logedin != null) {
+        res.json({
+            status: "signed in",
+            as: allUsers[logedin].email,
+            adminStatus: allUsers[logedin].adminStatus
+        });
+    } else {
+        res.json({
+            status: "unable to login"
+        })
+    }
 });
 
-//app.get("/login/:email/:password",function(req,res){
-//    console.log(req.params.email.toString());
-//
-//});
+app.get("/login", function (req, res) {
+    console.log("get login hitted");
+    res.end();
+});
 
 
-
-
-app.listen(port ,function(){
-    console.log("listening at: " + port)
+app.listen(3000, function () {
+    console.log("listening at: 3000")
 });
